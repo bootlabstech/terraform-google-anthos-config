d92cb89277618432aa25e4c23d8a63b3de7d795b
Silpashree Acharya <silpashree@bootlabstech.com>
2023-08-21 12:28:46 +0530

    latest provider.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_gke_hub_feature" "feature" {
-  name = var.gke_hub_feature_name
+  name     = var.gke_hub_feature_name
   location = var.gke_hub_feature_location
   provider = google-beta
-  project = var.project_id
-    lifecycle {
+  project  = var.project_id
+  lifecycle {
     ignore_changes = [labels]
   }
 }
